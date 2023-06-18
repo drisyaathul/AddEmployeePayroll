b77c2d1ad31bc07ce967bb042615145b66d2855f
drisyaathul <drisyak809@gmail.com>
2023-06-18 18:56:47 +0800

    Added: Reset the Employee Payroll Form

--- EmployeeQuery.js
+++ EmployeeQuery.js
@@ -98,6 +98,33 @@ const save = () => {
       localStorage.setItem("Employee Payroll List", JSON.stringify(employeePayrollList))
     }
 
+    //UC5:
+    const resetForm = () => {
+      setValue('#name',''); 
+      unsetSelectedValues('[name=profile]'); 
+      unsetSelectedValues('[name=gender]'); 
+      unsetSelectedValues('[name=department]'); 
+      setValue('#salary',''); 
+      setValue('#notes',''); 
+      setValue('#day','1'); 
+      setValue( '#month', 'January'); 
+      setValue('#year', '2020');}
+  
+  const unsetSelectedValues = (propertyValue) => {
+      let allItems = document.querySelectorall(propertyValue); 
+      allItems.forEach(item => {
+      item. checked = false; 
+      });
+  }
+  const setTextValue = (id, value) => {
+      const element = document.querySelector(id); 
+      element. textContent = value;
+  }
+  const setValue = (id, value) => {
+      const element = document.querySelector(id);
+      element.value = value;
+  }
+
 
 
 
